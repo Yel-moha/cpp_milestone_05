02fb7e21bd95f5fd4fef5df172746d33fe2c21fb
Youssef El Mohajir <yel-moha@c2r3p10.42firenze.it>
2026-09-12 20:20:14 +0200

    primi passi in cpp 05

---
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,18 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+    private:
+
+        const std::string name;
+        int grade;//A grade that ranges from 1 (highest possible grade) to 150 (lowest possible grade).
+
+    public:
+
+        
+
+};
+
+#endif
